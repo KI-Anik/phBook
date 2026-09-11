320a1e0b548cf39c080cdaa817f1963325c05194
KI-Anik <iamkhairul101@gmail.com>
2026-09-11 18:59:06 +0600

    Add comparison.js for basic JavaScript comparison practice

---
+++ d01/comparison.js
@@ -0,0 +1,50 @@
+// chapter: javascript Comparison; lesson: পাশের বাসার আন্টির তুলনা
+//basic comparison practice
+
+1.
+const meEatCake = 7;
+const broEatCake = 5;
+const compare = meEatCake > broEatCake;
+if (compare) {
+    // console.log("me is petuk");
+}
+else {
+    // console.log("bro is petuk");
+}
+
+2.
+const classFourStdnt = 75;
+const classFiveStdnt = 60;
+const highestStdnt = classFourStdnt > classFiveStdnt;
+const classCompare = highestStdnt ? "class four have highest student" : " class five have highest student";
+// console.log(classCompare);
+
+3.
+const myNum = 75;
+const broNum = 75;
+const isEqual = myNum == broNum;
+// console.log(isEqual);
+
+4.
+const target = 100;
+const savings = 95;
+const compareTarget = target <= savings ? "achived" : "not achived";
+// console.log(compareTarget);
+
+5.
+const ten = 10;
+const seven = 7;
+const comparison = ten != seven;
+// console.log(comparison);
+
+6.
+const twenty = 20;
+const fiften = 15;
+const compare1 = twenty <= fiften
+// console.log(compare1);
+
+7.
+const myScore = 45;
+const friendScore = 97;
+const compare2 = friendScore < myScore
+console.log(compare2);
