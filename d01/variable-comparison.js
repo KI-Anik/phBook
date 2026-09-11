74e6c23d057ba1b186b6c390d6b0b94c3f7c12e8
KI-Anik <iamkhairul101@gmail.com>
2026-09-11 20:05:51 +0600

    Add variable-comparison.js for JavaScript variable comparison practice

---
+++ d01/variable-comparison.js
@@ -0,0 +1,50 @@
+// chapter : ভেরিয়েবল তুলনা কখনো ভুলোনা
+//js variable comparison practice
+
+1.
+const broAge = 10;
+const myAge = 15;
+const compareAge = broAge <= myAge;
+// console.log(compareAge);
+
+2.
+const passMarks = 33;
+const myMarks = 45;
+const result = broMarks >= passMarks
+// console.log(result);
+
+3. 
+const jambura = 25;
+const kodbel = 36;
+const isCheaper = jambura < kodbel;
+// console.log(isCheaper);
+
+4. 
+const meRead = 10;
+const broRead = 10;
+const compare = meRead === broRead;
+// console.log(compare);
+
+5.
+const meBiscuit = 15;
+const broBiscuit = 20;
+const compare1 = meBiscuit < broBiscuit
+// console.log(compare1);
+
+6.
+const realMad = 2;
+const barsa = 3;
+const compare2 = realMad < barsa;
+// console.log(compare2);
+
+7.
+const myName = "Rahim";
+const broName = "karim";
+const compareName = myName === broName;
+// console.log(compareName);
+
+8.
+const meStudy = true;
+const broStudy = false;
+const comapreStudy = meStudy === broStudy;
+// console.log(comapreStudy);
